c706ec12a7692bedb6242a09b6a7f2ae0004bdb5
o-atsu <a0t5s0u1@gmail.com>
2020-05-01 18:36:08 +0900

    0501_1
    レイアウト考察

---
+++ Danmaku_MoriNaoki/Assets/particle_test/Dan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dan : MonoBehaviour
+{
+	private ParticleSystem ps;
+	public Transform me; 
+
+	void Awake(){
+		ps = GetComponent<ParticleSystem>();
+	}
+
+	void Update(){
+		ps.transform.Rotate(me.position);
+	}
+}
---
+++ Danmaku_MoriNaoki/Assets/particle_test/PITYU_NN.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PITYU_NN : MonoBehaviour
+{
+	void OnParticleCollision(GameObject obj){
+		Debug.Log("PITYU_NN" + obj.name);
+	}
+}
