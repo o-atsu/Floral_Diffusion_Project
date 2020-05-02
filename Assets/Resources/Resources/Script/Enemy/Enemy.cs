cc96468452c41d2ec449d59626db12bd32eb002e
scrapzero <doragontuinatomikku@gmail.com>
2020-05-03 02:57:52 +0900

    マージ

---
+++ Assets/Resources/Resources/Script/Enemy/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class Enemy : MonoBehaviour
+{
+	[SerializeField]
+	private int hp;
+	[SerializeField]
+	private Attack[] attacks;
+
+	/**** 倒されるときの処理 ****/
+	protected abstract void Defeated(); 
+
+	/**** 動きのコルーチン ****/
+	protected abstract IEnumerator move();
+	
+	//弾が当たった時にHPを減らす、HPが0以下なら倒される
+	private void Hit(){
+		Debug.Log("Hit");
+
+		hp--;
+		if(hp <= 0){
+			Defeated();
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		Hit();
+	}
+
+	void OnEnable(){
+		StartCoroutine("move");
+	}
+
+}
---
+++ Assets/Resources/Resources/Script/Enemy/wecary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wecary : Enemy
+{
+	private Rigidbody2D rb;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	protected override void Defeated(){
+		Debug.Log("wecary:Defeated!");
+	}
+
+	protected override IEnumerator move(){
+		while(true){
+			rb.velocity = new Vector3(2f, 0f, 0f);
+			yield return new WaitForSeconds(3f);
+			rb.velocity = new Vector3(-2f, 0f, 0f);
+			yield return new WaitForSeconds(3f);
+		}
+	}
+
+}
