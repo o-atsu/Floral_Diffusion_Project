eb03d04da9a9b262900fab8622fb96ef5747b2db
o-atsu <a0t5s0u1@gmail.com>
2020-05-04 17:57:49 +0900

    0504_1
    Enemyの実装

--- Danmaku_MoriNaoki/Assets/Resources/Script/Enemy/Enemy.cs
+++ Danmaku_MoriNaoki/Assets/Resources/Script/Enemy/Enemy.cs
@@ -5,31 +5,18 @@ using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
-	[SerializeField]
-	private int hp;
+	public int hp;
 	[SerializeField]
 	private Attack[] attacks;
 
 	/**** 倒されるときの処理 ****/
-	protected abstract void Defeated(); 
+	public virtual void Defeated(){
+		Destroy(gameObject);
+	} 
 
 	/**** 動きのコルーチン ****/
 	protected abstract IEnumerator move();
 	
-	//弾が当たった時にHPを減らす、HPが0以下なら倒される
-	private void Hit(){
-		Debug.Log("Hit");
-
-		hp--;
-		if(hp <= 0){
-			Defeated();
-		}
-	}
-	
-	void OnTriggerEnter2D(Collider2D other){
-		Hit();
-	}
-
 	void OnEnable(){
 		StartCoroutine("move");
 	}
--- Danmaku_MoriNaoki/Assets/Resources/Script/Enemy/wecary.cs
+++ Danmaku_MoriNaoki/Assets/Resources/Script/Enemy/wecary.cs
@@ -10,7 +10,7 @@ public class wecary : Enemy
 		rb = GetComponent<Rigidbody2D>();
 	}
 
-	protected override void Defeated(){
+	public override void Defeated(){
 		Debug.Log("wecary:Defeated!");
 	}
 
