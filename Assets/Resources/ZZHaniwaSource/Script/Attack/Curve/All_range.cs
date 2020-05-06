009c0341e3cae7c306236e431b0b9d6e6b261680
scrapzero <doragontuinatomikku@gmail.com>
2020-05-06 23:33:35 +0900

    マージ2

---
+++ Assets/Resources/ZZHaniwaSource/Script/Attack/Curve/All_range.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class All_range : Attack
+{
+	[SerializeField]
+	private All_range_generator generator;
+	[SerializeField]
+	private float angle_gap = 0f;
+	[SerializeField]
+	private int shot_time = 2;
+
+	protected override IEnumerator shot(){
+		while(true){
+			if(shot_time % 2 == 1){
+				generator.SetStatus(angle_gap, 0);
+				generator.Generate();
+			}
+			for(int i = 1;i <= shot_time / 2;i++){
+				generator.SetStatus(angle_gap, i);
+				generator.Generate();
+				generator.SetStatus(angle_gap, -i);
+				generator.Generate();
+			}
+		yield return new WaitForSeconds(interval);
+		}
+	}
+
+	void Start(){
+		StartCoroutine("shot");
+	}
+}
