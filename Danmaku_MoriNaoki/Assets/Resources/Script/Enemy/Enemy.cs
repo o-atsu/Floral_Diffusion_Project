e06319c01b9a3adcd61d52cc6a043943a9c9d5b2
o-atsu <a0t5s0u1@gmail.com>
2020-05-09 22:10:13 +0900

    0509_2
    EnemyクラスのGet_percent実装

--- Danmaku_MoriNaoki/Assets/Resources/Script/Enemy/Enemy.cs
+++ Danmaku_MoriNaoki/Assets/Resources/Script/Enemy/Enemy.cs
@@ -30,8 +30,8 @@ public abstract class Enemy : MonoBehaviour
 		hp = MAX_HP;
 	}
 
-	public int Get_MAX_HP(){
-		return MAX_HP;
+	public float Get_percent(){
+		return 100f * (float)hp / (float)MAX_HP;
 	}
 
 	public void Hit(int damage){
@@ -39,8 +39,6 @@ public abstract class Enemy : MonoBehaviour
 		if(hp <= 0){
 			Defeated();
 		}
-
-		//Debug.Log(hp);
 	}
 
 }
