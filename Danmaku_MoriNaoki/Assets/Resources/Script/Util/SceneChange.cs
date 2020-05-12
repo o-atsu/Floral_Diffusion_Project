21a7396cb01397799a38d4b8caf7c572bfba0d75
o-atsu <a0t5s0u1@gmail.com>
2020-05-12 21:42:47 +0900

    0512_1
    SceneChange実装

---
+++ Danmaku_MoriNaoki/Assets/Resources/Script/Util/SceneChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public string next_scene;
+
+	public IEnumerator Load_scene(){
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(next_scene);
+		while(!asyncLoad.isDone){
+			yield return null;
+		}
+	}
+}
